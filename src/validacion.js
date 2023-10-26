3fab0e2d21cbc27ba019c8e6e11fc280441ff008
EMRocioB <rocioburguener@gmail.com>
2023-10-25 21:18:49 -0300

    Metodo validacionLibro

--- src/validacion.js
+++ src/validacion.js
@@ -1,2 +1,16 @@
+function validacionLibro(libro) {
+    if (!libro.nombre || !libro.autor || !libro.categoria || !libro['año-publicacion'] || !libro.ISBN) {
+        throw new Error("Datos incompletos. Asegúrese de proporcionar nombre, autor, categoria año de publicacion e ISBN.");
+    }
+  
+    const validFields = ["id", "nombre", "autor", "categoria", "año-publicacion", "ISBN"];
+    const incomingFields = Object.keys(libro);
+    for (let field of incomingFields) {
+        if (!validFields.includes(field)) {
+            throw new Error(`Campo no permitido: ${field}`);
+        }
+    }
+  }
 
+  export default validacionLibro;
 
